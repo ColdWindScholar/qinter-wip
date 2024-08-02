78a0a8b3348bd4c882413f50c479ce2330987261
Ookiineko <chiisaineko@protonmail.com>
2024-08-03 02:33:15 +0900

    feat: messagebox.show{info,warning,error}
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

---
+++ _qinter/msgbox.py
@@ -0,0 +1,17 @@
+from PySide6.QtWidgets import QMessageBox
+
+
+_R_OK = 'ok'
+
+
+def _showxxx_func(fn):
+  def inner(title: str = '',
+            message: str = '') -> str:
+    fn(None, title, message)
+    return _R_OK
+  return inner
+
+
+showinfo_impl = _showxxx_func(QMessageBox.information)
+showwarning_impl = _showxxx_func(QMessageBox.warning)
+showerror_impl = _showxxx_func(QMessageBox.critical)
---
+++ tkinter/messagebox.py
@@ -0,0 +1,3 @@
+from _qinter.msgbox import showinfo_impl as showinfo
+from _qinter.msgbox import showwarning_impl as showwarning
+from _qinter.msgbox import showerror_impl as showerror
